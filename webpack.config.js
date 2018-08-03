8425a7f1054b932715b91e1feb49194e07533b72
Cosmin Seceleanu <coosmin26@yahoo.com>
2018-08-03 22:51:35 +0300

    fix building app

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,31 @@
 const path = require('path');
 
+const libraryName = 'library';
 const config = {
 	entry: './index.js',
 	output: {
-		filename: '[name].js',
+		filename: 'index.js',
 		path: path.resolve(__dirname, 'dist'),
+		library: libraryName,
+		libraryTarget: 'umd',
+		umdNamedDefine: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
 	},
 };
 
 module.exports = (env, argv) => {
 	if (argv.mode === 'production') {
-		config.output.filename = '[name].min.js';
+		config.output.filename = 'index.min.js';
 	}
 
 	return config;
